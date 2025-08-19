3785c37ba30be65e8b495246b9e3fd4554909aa2
Nick Kosarev <hmbanan666@hotmail.com>
2025-08-19 09:44:12 +0200

    chore: sign in page rework (#75)

--- apps/web-app/shared/services/common.ts
+++ apps/web-app/shared/services/common.ts
@@ -5,3 +5,7 @@ export type WeightUnit = typeof weightUnitSchema.infer
 
 export const userGenderSchema = type('"male" | "female" | "unknown"')
 export type UserGender = typeof userGenderSchema.infer
+
+export const signInSchema = type({
+  phone: type('11 <= string <= 16').describe('error.length.invalid'),
+})
