6309cdc9a8d6c7d3cf7de0faf492b3cba1398c78
Nick Kosarev <hmbanan666@hotmail.com>
2025-08-21 14:08:08 +0200

    feat: notify about beacon in telegram

--- apps/web-app/server/api/epic/comment/id/[commentId]/beacon.post.ts
+++ apps/web-app/server/api/epic/comment/id/[commentId]/beacon.post.ts
@@ -1,5 +1,6 @@
 import { repository } from '@roll-stack/database'
 import { type } from 'arktype'
+import { useAtriumBot } from '~~/server/services/telegram/atrium-bot'
 import { createBeaconSchema } from '~~/shared/services/notification'
 
 export default defineEventHandler(async (event) => {
@@ -26,6 +27,8 @@ export default defineEventHandler(async (event) => {
       throw data
     }
 
+    const { telegram } = useRuntimeConfig()
+
     const comment = await repository.epic.findComment(commentId)
     if (!comment) {
       throw createError({
@@ -34,9 +37,16 @@ export default defineEventHandler(async (event) => {
       })
     }
 
+    const epic = await repository.epic.find(comment.epicId)
     const users = await repository.user.list()
     const commentAuthor = users.find((user) => user.id === comment.userId)
-    const epic = await repository.epic.find(comment.epicId)
+    const sender = users.find((user) => user.id === session.user?.id)
+    if (!sender) {
+      throw createError({
+        statusCode: 404,
+        message: 'User not found',
+      })
+    }
 
     // Create beacon
     for (const userId of data.usersId) {
@@ -45,15 +55,24 @@ export default defineEventHandler(async (event) => {
         continue
       }
 
+      const title = `${suffixByGender(['Создал', 'Создала'], sender?.gender)} маяк в эпике «${epic?.title}»`
+      const description = `${commentAuthor?.name} ${commentAuthor?.surname} ${suffixByGender(['оставил', 'оставила'], commentAuthor?.gender)} комментарий: ${comment.text}`
+
       await repository.notification.create({
         type: 'user_beacon_on_epic_comment_created',
         userId,
-        authorId: session.user.id,
+        authorId: sender.id,
         epicId: comment.epicId,
         epicCommentId: comment.id,
-        title: `${suffixByGender(['Создал', 'Создала'], user.gender)} маяк в эпике «${epic?.title}»`,
-        description: `${commentAuthor?.name} ${commentAuthor?.surname} ${suffixByGender(['оставил', 'оставила'], commentAuthor?.gender)} комментарий: ${comment.text}`,
+        title,
+        description,
       })
+
+      // Telegram - Atrium
+      const atriumUser = user.telegramUsers.find((u) => u.botId === telegram.atriumBotId)
+      if (atriumUser) {
+        await useAtriumBot().api.sendMessage(atriumUser.telegramId, `👋 ${sender.name} ${sender.surname}\n${title}\n\n${description}`)
+      }
     }
 
     return {
