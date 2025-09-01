b6ada81937e60fa004d8c37c7bfdd66cdc58a176
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-01 15:29:57 +0200

    feat: request contact (#116)

--- apps/web-app/server/services/telegram/order-bot.ts
+++ apps/web-app/server/services/telegram/order-bot.ts
@@ -1,3 +1,4 @@
+import type { Client } from '@roll-stack/database'
 import type { Context } from 'grammy'
 import { createId } from '@paralleldrive/cuid2'
 import { repository } from '@roll-stack/database'
@@ -22,6 +23,11 @@ export async function useCreateOrderBot() {
     }
 
     return handleMessage(ctx)
+  })
+
+  // User shared contact
+  bot.on('message:contact', async (ctx) => {
+    return handleContact(ctx)
   })
 
   // Somebody invited bot to a group
@@ -52,31 +58,18 @@ async function handleStart(ctx: Context) {
   // Find user
   const telegramUser = await repository.telegram.findClientByTelegramIdAndBotId(ctx.message.from.id.toString(), telegram.orderBotId)
   if (!telegramUser) {
-    // Create Client
-    const id = createId()
-    const client = await repository.client.create({
-      id,
-      name: ctx.message.from.first_name,
-      surname: ctx.message.from.last_name,
-      avatarUrl: `https://atrium.sushi-love.ru/api/avatar/${id}.svg?emotion=7&gender=female`,
-    })
-
-    // Create User
-    const accessKey = createId()
-    const createdUser = await repository.telegram.createUser({
-      telegramUserType: ctx.message.chat.type,
-      telegramId: ctx.message.from.id.toString(),
-      firstName: ctx.message.from.first_name,
-      lastName: ctx.message.from.last_name,
-      username: ctx.message.from.username,
-      accessKey,
-      botId: telegram.atriumBotId,
-      clientId: client.id,
-    })
-
-    logger.log('new user', createdUser?.id, ctx.message.from.id, ctx.message.text)
-
-    await ctx.reply('Успех! Теперь вы можете совершать заказы.')
+    // Get phone number
+    await ctx.reply(
+      'Всего один шаг - подтвердите номер телефона. 📱👇\n\n*Продолжая, вы подтверждаете своё согласие на [сбор и обработку персональных данных](https://sushi-love.ru), а также принимаете условия [Пользовательского соглашения](https://sushi-love.ru)*',
+      {
+        parse_mode: 'MarkdownV2',
+        reply_markup: {
+          keyboard: [[{ text: 'Подтвердить номер', request_contact: true }]],
+          one_time_keyboard: true,
+          resize_keyboard: true,
+        },
+      },
+    )
     return
   }
 
@@ -86,6 +79,39 @@ async function handleStart(ctx: Context) {
   }
 
   await ctx.reply('Вы уже авторизованы.')
+}
+
+async function handleContact(ctx: Context) {
+  if (!ctx.message?.contact) {
+    return
+  }
+
+  // Not private chat?
+  if (ctx.message.chat.type !== 'private') {
+    await ctx.reply('Я пока не умею отвечать на групповые сообщения.')
+    return
+  }
+
+  // Find Client
+  const client = await findOrCreateClient(ctx.message.contact.phone_number, {
+    name: ctx.message.from.first_name,
+    surname: ctx.message.from.last_name,
+  })
+
+  const telegramUser = await repository.telegram.createUser({
+    telegramUserType: ctx.message.chat.type,
+    telegramId: ctx.message.from.id.toString(),
+    firstName: ctx.message.from.first_name,
+    lastName: ctx.message.from.last_name,
+    username: ctx.message.from.username,
+    botId: telegram.orderBotId,
+    accessKey: createId(),
+    clientId: client.id,
+  })
+
+  logger.log('new user', telegramUser)
+
+  await ctx.reply('Успех! Теперь вы можете совершать заказы.')
 }
 
 async function handleMessage(ctx: Context) {
@@ -108,4 +134,20 @@ export function useOrderBot(): Bot {
   }
 
   return bot
+}
+
+async function findOrCreateClient(phone: string, user: { name: string, surname: string | undefined }): Promise<Client> {
+  const client = await repository.client.findByPhone(phone)
+  if (!client) {
+    const id = createId()
+    return repository.client.create({
+      id,
+      phone,
+      name: user.name,
+      surname: user.surname,
+      avatarUrl: `https://atrium.sushi-love.ru/api/avatar/${id}.svg?emotion=7&gender=female`,
+    })
+  }
+
+  return client
 }
