4c6d120cf2b281372651b1f7e78a80512db84949
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-02 12:38:03 +0200

    chore: navigation rework

--- apps/storefront-telegram/app/components/Navigation.vue
+++ apps/storefront-telegram/app/components/Navigation.vue
@@ -2,40 +2,22 @@
   <nav class="z-50 touch-pan-x fixed bottom-0 left-0 right-0 w-full h-25 tg-bg-bottom-bar border-t border-default rounded-t-lg motion-preset-slide-up">
     <div class="max-w-[28rem] mx-auto">
       <div class="mt-3 grid grid-cols-3">
-        <button
+        <NavigationButton
           v-for="route in mainRoutes"
           :key="route.path"
-          class="flex flex-col items-center justify-center gap-1 px-4 cursor-pointer tg-text-subtitle"
-          @click="handleClick(route.path)"
-        >
-          <div
-            class="relative py-1 w-full rounded-2xl flex flex-row items-center justify-center"
-            :class="[
-              (route.exact ? router.currentRoute.value.path === route.path : router.currentRoute.value.path.startsWith(route.path)) && 'tg-bg-button tg-text-button motion-translate-y-in',
-            ]"
-          >
-            <UIcon :name="route.icon" class="size-6" />
-          </div>
-          <p
-            class="text-xs font-medium"
-            :class="[
-              (route.exact ? router.currentRoute.value.path === route.path : router.currentRoute.value.path.startsWith(route.path)) && 'tg-text',
-            ]"
-          >
-            {{ route.title }}
-          </p>
-        </button>
+          :route="route"
+        />
       </div>
     </div>
   </nav>
 </template>
 
 <script setup lang="ts">
-const { t } = useI18n()
-const { vibrate } = useFeedback()
-const router = useRouter()
+import type { NavigationRoute } from '#shared/types/index'
 
-const mainRoutes = computed(() => [
+const { t } = useI18n()
+
+const mainRoutes = computed<NavigationRoute[]>(() => [
   {
     path: '/',
     name: 'home',
@@ -56,9 +38,4 @@ const mainRoutes = computed(() => [
     icon: 'i-lucide-menu',
   },
 ])
-
-function handleClick(path: string) {
-  vibrate()
-  router.push(path)
-}
 </script>
---
+++ apps/storefront-telegram/shared/types/index.ts
@@ -0,0 +1,7 @@
+export type NavigationRoute = {
+  path: string
+  name: string
+  title: string
+  icon: string
+  exact?: boolean
+}
