5037f56e851b81d46a1ca7952ac16e96f1a753fd
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-03 12:26:37 +0200

    feat: card with parallax

--- apps/storefront-telegram/app/stores/client.ts
+++ apps/storefront-telegram/app/stores/client.ts
@@ -1,10 +1,11 @@
 import { initDataRaw as _initDataRaw, initDataState as _initDataState, useSignal } from '@telegram-apps/sdk-vue'
+import { parsePhoneNumberWithError } from 'libphonenumber-js'
 
 export const useClientStore = defineStore('client', () => {
   const id = ref<string | undefined>(undefined)
   const name = ref<string | undefined>(undefined)
   const surname = ref<string | undefined>(undefined)
-  // const phone = ref<string | null>(null)
+  const phone = ref<string | undefined>(undefined)
   const avatarUrl = ref<string | null>(null)
 
   const initDataRaw = useSignal(_initDataRaw)
@@ -12,6 +13,16 @@ export const useClientStore = defineStore('client', () => {
 
   const fullName = computed(() => {
     return `${name.value} ${surname.value}`
+  })
+
+  const formattedPhone = computed(() => {
+    if (!phone.value) {
+      return ''
+    }
+
+    const phoneNumber = parsePhoneNumberWithError(phone.value, 'RU')
+
+    return phoneNumber?.format('INTERNATIONAL')
   })
 
   async function update() {
@@ -26,9 +37,9 @@ export const useClientStore = defineStore('client', () => {
       }
 
       id.value = data.id
+      phone.value = data.phone
       name.value = data.name
       surname.value = data.surname ?? ''
-      // phone.value = data.phone
       avatarUrl.value = data.avatarUrl
     } catch (error) {
       if (error instanceof Error) {
@@ -68,11 +79,13 @@ export const useClientStore = defineStore('client', () => {
 
   return {
     id,
+    phone,
     name,
     surname,
     avatarUrl,
 
     fullName,
+    formattedPhone,
 
     initDataRaw,
     initDataState,
