6ddf549b7fb3c46dda8fd03fb8ea381edb9fde72
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-03 12:31:58 +0200

    feat: card with parallax (#126)

--- apps/storefront-telegram/app/components/UserPointsCard.vue
+++ apps/storefront-telegram/app/components/UserPointsCard.vue
@@ -1,16 +1,58 @@
 <template>
-  <div class="aspect-3/2 bg-primary rounded-lg">
-    <div class="w-full h-full p-4 flex flex-col justify-between tg-text-button">
-      <div class="text-lg/5 font-semibold">
-        {{ clientStore.fullName }}
+  <div ref="target" class="relative w-full h-auto aspect-3/2 perspective-normal motion-preset-slide-down">
+    <div
+      class="absolute inset-0 bg-primary rounded-lg"
+      :style="{
+        transform: `rotateX(${parallax.roll * 3}deg) rotateY(${parallax.tilt * 3}deg)`,
+      }"
+    />
+
+    <div
+      class="z-10 w-full h-full p-4 flex flex-col justify-between tg-text-button"
+      :style="{
+        transform: `rotateX(${parallax.roll * 2}deg) rotateY(${parallax.tilt * 2}deg)`,
+      }"
+    >
+      <div class="flex flex-row justify-between items-center">
+        <div class="flex flex-col gap-3">
+          <div class="text-lg/5 font-medium">
+            Карта лояльности
+          </div>
+          <div class="flex flex-row gap-1.5 items-center">
+            <p class="text-3xl/5 font-semibold">
+              680
+            </p>
+            <UIcon name="fluent:heart-circle-24-filled" class="size-6" />
+          </div>
+        </div>
+
+        <img
+          src="/sushi-heart.svg"
+          alt=""
+          class="w-12 opacity-20 invert-100"
+        >
       </div>
-      <div class="text-3xl/5 font-bold">
-        5%
+
+      <div class="flex flex-row justify-between items-center">
+        <div class="flex flex-row gap-2 items-center">
+          <div class="px-3.5 py-1.5 text-2xl/5 text-primary font-bold ring ring-default bg-default rounded-2xl">
+            5%
+          </div>
+
+          <div>
+            Кешбэк
+          </div>
+        </div>
+
+        <UIcon name="i-lucide-info" class="size-6" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const clientStore = useClientStore()
+import { useParallax } from '@vueuse/core'
+
+const target = useTemplateRef<HTMLElement>('target')
+const parallax = reactive(useParallax(target))
 </script>
--- apps/storefront-telegram/app/pages/user.vue
+++ apps/storefront-telegram/app/pages/user.vue
@@ -1,13 +1,35 @@
 <template>
   <PageContainer>
+    <div class="flex flex-col gap-1">
+      <h1 class="text-xl/5 font-bold">
+        {{ clientStore.fullName }}
+      </h1>
+      <p v-if="clientStore.formattedPhone" class="text-muted text-sm/5 font-medium">
+        {{ clientStore.formattedPhone }}
+      </p>
+    </div>
+
     <UserPointsCard />
 
-    <div class="flex flex-row gap-3.5 items-center">
-      <div class="flex flex-col gap-1">
-        <h2 class="text-lg/5 font-bold">
-          Тут будет меню
-        </h2>
-      </div>
+    <div class="flex flex-col gap-1">
+      <UButtonGroup orientation="vertical" size="xl">
+        <UButton
+          color="neutral"
+          variant="subtle"
+          label="Мои заказы"
+          icon="i-lucide-shopping-basket"
+        />
+        <UButton
+          color="neutral"
+          variant="outline"
+          label="Мои адреса"
+          icon="i-lucide-map-pin-house"
+        />
+      </UButtonGroup>
     </div>
   </PageContainer>
 </template>
+
+<script setup lang="ts">
+const clientStore = useClientStore()
+</script>
