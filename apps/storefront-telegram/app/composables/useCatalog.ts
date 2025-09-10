31cb13dc077fc8a32d29428ade764edddc1753b6
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-10 15:11:05 +0200

    chore: some rework (#146)

--- apps/storefront-telegram/app/composables/useCatalog.ts
+++ apps/storefront-telegram/app/composables/useCatalog.ts
@@ -1,14 +1,31 @@
 function _useCatalog() {
   const visibleCategory = ref<string | null>(null)
+  const lastVisibleCategory = ref<string | null>(null)
 
   const observerOptions = { rootMargin: '0px 0px 200px 0px' }
 
-  const scrollToCategoryInHorizontalMenu = useDebounceFn(() => {
-    const category = window.document.getElementById(`to-${visibleCategory.value}`)
-    category?.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
-  }, 800)
+  function scrollToCategoryInHorizontalMenu() {
+    if (!visibleCategory.value || lastVisibleCategory.value === visibleCategory.value) {
+      return
+    }
 
-  watch(visibleCategory, () => scrollToCategoryInHorizontalMenu())
+    lastVisibleCategory.value = visibleCategory.value
+
+    const category = window.document.getElementById(`to-${visibleCategory.value}`)
+    if (!category) {
+      return
+    }
+
+    category.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'start' })
+  }
+
+  onMounted(() => {
+    // Activate scroll watcher after some time
+    setTimeout(() => {
+      lastVisibleCategory.value = visibleCategory.value
+      setInterval(scrollToCategoryInHorizontalMenu, 250)
+    }, 2000)
+  })
 
   function scrollToCategory(slug: string) {
     const category = window.document.getElementById(slug)
