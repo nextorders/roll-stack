15f9b771d3ce5364ac3ea1e9955a07a941cbf347
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-16 17:47:45 +0200

    chore: some updates (#165)

--- apps/web-app/server/tasks/ai/daily-report.ts
+++ apps/web-app/server/tasks/ai/daily-report.ts
@@ -3,7 +3,6 @@ import { repository } from '@roll-stack/database'
 import { format } from 'date-fns'
 import { ru } from 'date-fns/locale/ru'
 import OpenAI from 'openai'
-import { useAtriumBot } from '~~/server/services/telegram/atrium-bot'
 
 const logger = useLogger('task:ai:daily-report')
 
@@ -19,7 +18,7 @@ export default defineTask({
     }
 
     try {
-      const { ai, telegram } = useRuntimeConfig()
+      const { ai } = useRuntimeConfig()
 
       const tasks = await repository.task.listCompletedToday()
       const preparedTasks = tasks.map((task) => ({
@@ -55,30 +54,30 @@ export default defineTask({
 
       // Flow item
       const date = format(new Date(), 'd MMMM', { locale: ru })
-      const flowItem = await repository.flow.createItem({
+      await repository.flow.createItem({
         type: 'daily_task_report',
         title: `Задачи ${date}`,
         description: finalMessage,
       })
 
-      const separator = 'zzzzz'
-      const startAppData = `flow${separator}${flowItem?.id}`
+      // const separator = 'zzzzz'
+      // const startAppData = `flow${separator}${flowItem?.id}`
 
-      // Get first words
-      const messageIntro = finalMessage.split(' ').slice(0, 40).join(' ')
-      const preparedMessage = `${messageIntro}...\n\nПродолжение внутри Атриума? 🙃`
+      // // Get first words
+      // const messageIntro = finalMessage.split(' ').slice(0, 40).join(' ')
+      // const preparedMessage = `${messageIntro}...\n\nПродолжение внутри Атриума? 🙃`
 
-      await useAtriumBot().api.sendMessage(telegram.teamGroupId, preparedMessage, {
-        link_preview_options: {
-          is_disabled: true,
-        },
-        reply_markup: {
-          inline_keyboard: [[{
-            text: '👉 Открыть Атриум',
-            url: `https://t.me/sushi_atrium_bot/app?startapp=${startAppData}`,
-          }]],
-        },
-      })
+      // await useAtriumBot().api.sendMessage(telegram.teamGroupId, preparedMessage, {
+      //   link_preview_options: {
+      //     is_disabled: true,
+      //   },
+      //   reply_markup: {
+      //     inline_keyboard: [[{
+      //       text: '👉 Открыть Атриум',
+      //       url: `https://t.me/sushi_atrium_bot/app?startapp=${startAppData}`,
+      //     }]],
+      //   },
+      // })
     } catch (error) {
       errorResolver(error)
     }
