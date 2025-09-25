cbc60941bd3ccc060dee46df179176c0c1057409
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-25 14:02:38 +0200

    feat: webinar app

---
+++ apps/webinar/app/app.vue
@@ -0,0 +1,36 @@
+<template>
+  <UApp
+    :locale="locales[locale]"
+    class="min-h-svh"
+  >
+    <NuxtLoadingIndicator :color="false" class="bg-primary h-[2px]" />
+    <NuxtPage />
+  </UApp>
+</template>
+
+<script setup lang="ts">
+import * as locales from '@nuxt/ui/locale'
+
+const { locale } = useI18n()
+
+const lang = computed(() => locales[locale.value].code)
+const dir = computed(() => locales[locale.value].dir)
+
+useHead({
+  htmlAttrs: {
+    lang,
+    dir,
+  },
+})
+
+// Fix system theme
+const colorMode = useColorMode()
+watch(colorMode, () => {
+  const colorModeStorage = localStorage.getItem('color-mode')
+
+  if (colorMode.value === 'system' || colorModeStorage === 'system') {
+    colorMode.value = 'light'
+    localStorage.setItem('color-mode', 'light')
+  }
+})
+</script>
