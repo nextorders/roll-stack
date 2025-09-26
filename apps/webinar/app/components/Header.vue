56541c56c72c1f76be94c270bb7f0868eed4c74a
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-26 11:19:11 +0200

    chore: webinar new block (#186)

--- apps/webinar/app/components/Header.vue
+++ apps/webinar/app/components/Header.vue
@@ -1,25 +1,6 @@
 <template>
   <UContainer class="mt-10 flex flex-col md:flex-row gap-y-6 items-center justify-between">
     <HeaderLogo />
-
-    <div class="shrink-0">
-      <div class="flex flex-row gap-2">
-        <UColorModeButton size="xl" />
-        <UButton
-          to="https://t.me/SLFranchiseBot"
-          size="xl"
-          color="neutral"
-          variant="ghost"
-          icon="simple-icons:telegram"
-        />
-        <UButton
-          to="https://vk.com/franshizasushi"
-          size="xl"
-          color="neutral"
-          variant="ghost"
-          icon="simple-icons:vk"
-        />
-      </div>
-    </div>
+    <SocialButtons />
   </UContainer>
 </template>
