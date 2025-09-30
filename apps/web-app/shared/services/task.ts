5892df785cd1ed24d4255fdc11cb4c335ee7a8f8
Nick Kosarev <hmbanan666@hotmail.com>
2025-09-30 10:05:40 +0200

    chore: description length (#193)

--- apps/web-app/shared/services/task.ts
+++ apps/web-app/shared/services/task.ts
@@ -5,7 +5,7 @@ export type Resolution = typeof resolutionSchema.infer
 
 export const createTaskSchema = type({
   name: type('2 <= string <= 150').describe('error.length.invalid'),
-  description: type('string <= 500 | undefined').describe('error.length.invalid').optional(),
+  description: type('string <= 2000 | undefined').describe('error.length.invalid').optional(),
   performerId: type('string | undefined | null').describe('error.length.invalid').optional(),
   date: type('string | undefined | null').describe('error.length.invalid').optional(),
   listId: type('string').describe('error.length.invalid'),
@@ -14,7 +14,7 @@ export type CreateTask = typeof createTaskSchema.infer
 
 export const updateTaskSchema = type({
   name: type('2 <= string <= 150').describe('error.length.invalid').optional(),
-  description: type('string <= 500 | undefined').describe('error.length.invalid').optional(),
+  description: type('string <= 2000 | undefined').describe('error.length.invalid').optional(),
   performerId: type('string | undefined | null').describe('error.length.invalid').optional(),
   date: type('string | undefined | null').describe('error.length.invalid').optional(),
   listId: type('string | undefined').describe('error.length.invalid').optional(),
